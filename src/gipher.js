f3526033163c3a65048fd87b9818d641a6650d00
Nathan Icart <nathan.icart@gmail.com>
2017-07-22 06:30:47 -0500

    fixed gif randomizer, improved aesthetics

--- src/gipher.js
+++ src/gipher.js
@@ -2,12 +2,8 @@ var api = 'https://api.giphy.com/v1/gifs/';
 var query = 'random?';
 var apiKey = 'api_key=a89fcdd94f6e49bfabea496708aa14d1';
 var rating = '&tag=&rating=R';
-var limit = '&limit=1&offset=0&rating=R&lang=en';
+var limit = '&limit=15&offset=0&rating=R&lang=en';
 
-// if user uses the search button set query = 'search?'
-// if user uses the search button set rating = '&limit=1&offset=0&rating=R&lang=en'
-// format for search string '&q=search term'
-// full url with search = api + query + apiKey + searchTerm + rating
 
 window.onload = function() {
 
@@ -16,26 +12,30 @@ window.onload = function() {
   
   //function that gets the JSON from the url, and posts its data as an img
   let imgGrab = (search) => {
-
+ 
+    //without a search string, api will pull and parse JSON
+    //with a search string, url will be compiled, then it will retrieve and parse JSON
   	if (search === undefined) {
-      var url = api + query + apiKey + rating;    
+      var url = api + query + apiKey + rating;
+      var gifJSON = $.getJSON(url);    
     } else {
-    	var searchData = 'https://api.giphy.com/v1/gifs/search?api_key=a89fcdd94f6e49bfabea496708aa14d1&q=ryan gosling&limit=1&offset=0&rating=R&lang=en';
+    	var searchData = api + 'search?' + apiKey + '&q=' + search + limit;
+    	var gifJSON = $.getJSON(searchData);
     }
 
-    var gifJSON = $.getJSON(url);
-    
+    //takes parsed JSON, the url within an img tag
     gifJSON.done((data) => { 
     	if (searchData) {
-    	  document.getElementById("gif-displayer").innerHTML = '<img src="' + data.data[0].images.original.url + '">';
+    		//with the current settings, 15 image urls will be pulled into an array
+    		//this randomizes which image will be displayed based on the length of the image array
+    		var randomizer = Math.floor(Math.random() * data.data.length);
+    	  document.getElementById("gif-displayer").innerHTML = '<img src="' + data.data[randomizer].images.original.url + '">';
       } else {
     	  document.getElementById("gif-displayer").innerHTML = '<img src="' + data.data.image_url + '">'; 
       }
     });
 
   };
-
-  imgGrab();
 
   //onclick, grabs search string and executes search
   document.getElementById("search-gif").onclick = () => {activateSearch()};
@@ -44,5 +44,8 @@ window.onload = function() {
   let activateSearch = () => {
     imgGrab(document.getElementById("search-string").value);
   }
+
+//automatically runs gipher onload and displays a random gif
+imgGrab();
 
 }
