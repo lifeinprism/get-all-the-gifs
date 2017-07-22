faae9fbd33dcf48f9c913cb975425dd057c08ce0
Nathan Icart <nathan.icart@gmail.com>
2017-07-22 01:30:02 -0500

    added second button, css, and text box

--- src/gipher.js
+++ src/gipher.js
@@ -2,13 +2,20 @@ var api = 'https://api.giphy.com/v1/gifs/';
 var query = 'random?';
 var apiKey = 'api_key=a89fcdd94f6e49bfabea496708aa14d1';
 var rating = '&tag=&rating=R';
-//var gifButton = document.getElementById("serve-gif");
+var searchTerm = '';
+
+// if user uses the search button set query = 'search?'
+// if user uses the search button set rating = '&limit=1&offset=0&rating=R&lang=en'
+// format for search string '&q=search term'
+// full url with search = api + query + apiKey + searchTerm + rating
 
 window.onload = function() {
 
+  //serves random gif onclick
   document.getElementById("serve-gif").onclick = () => {imgGrab()};
   
-  let imgGrab = () => {
+  //function that gets the JSON from the url, and posts its data as an img
+  let imgGrab = (search) => {
     var url = api + query + apiKey + rating;
     var gifJSON = $.getJSON(url);
     
@@ -19,5 +26,13 @@ window.onload = function() {
   };
 
   imgGrab();
+
+  //onclick, grabs search string and executes search
+  document.getElementById("search-gif").onclick = () => {activateSearch()};
+  
+  //runs imgGrab based on search entry
+  let activateSearch = () => {
+    console.log(document.getElementById("search-string").value);
+  }
 
 }
