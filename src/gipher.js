860d480851032fffff17402ec7af62dcf5a716a3
Nathan Icart <nathan.icart@gmail.com>
2017-07-22 05:54:11 -0500

    testing search function

--- src/gipher.js
+++ src/gipher.js
@@ -2,7 +2,7 @@ var api = 'https://api.giphy.com/v1/gifs/';
 var query = 'random?';
 var apiKey = 'api_key=a89fcdd94f6e49bfabea496708aa14d1';
 var rating = '&tag=&rating=R';
-var searchTerm = '';
+var limit = '&limit=1&offset=0&rating=R&lang=en';
 
 // if user uses the search button set query = 'search?'
 // if user uses the search button set rating = '&limit=1&offset=0&rating=R&lang=en'
@@ -16,11 +16,21 @@ window.onload = function() {
   
   //function that gets the JSON from the url, and posts its data as an img
   let imgGrab = (search) => {
-    var url = api + query + apiKey + rating;
+
+  	if (search === undefined) {
+      var url = api + query + apiKey + rating;    
+    } else {
+    	var searchData = 'https://api.giphy.com/v1/gifs/search?api_key=a89fcdd94f6e49bfabea496708aa14d1&q=ryan gosling&limit=1&offset=0&rating=R&lang=en';
+    }
+
     var gifJSON = $.getJSON(url);
     
     gifJSON.done((data) => { 
-    	document.getElementById("gif-displayer").innerHTML = '<img src="' + data.data.image_url + '">'; 
+    	if (searchData) {
+    	  document.getElementById("gif-displayer").innerHTML = '<img src="' + data.data[0].images.original.url + '">';
+      } else {
+    	  document.getElementById("gif-displayer").innerHTML = '<img src="' + data.data.image_url + '">'; 
+      }
     });
 
   };
@@ -32,7 +42,7 @@ window.onload = function() {
   
   //runs imgGrab based on search entry
   let activateSearch = () => {
-    console.log(document.getElementById("search-string").value);
+    imgGrab(document.getElementById("search-string").value);
   }
 
 }
